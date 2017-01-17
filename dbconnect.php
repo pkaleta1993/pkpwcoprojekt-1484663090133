67306f06830b68dd67f0f263e214459f340dbb41
Paweł Kaleta <pkaleta1993@gmail.com>
2017-01-17 19:37:57 +0000

    db

--- dbconnect.php
+++ dbconnect.php
@@ -5,7 +5,9 @@ Plik odpowiadający za połączenie z bazą mySQL na podstawie konfiguracji zawa
 
 require_once "config.php"; # Deklaracja zawartości pliku "config.php".
 //include "config.php"; # Deklaracja zawartości pliku "config.php".
+echo "test w dbconnect1";
 $connection = mysql_connect(db_server,db_user,db_pass); # Połączenie z serwerem bazy danych za pomocą loginu i hasła.
+echo "test w dbconnect2";
 $db = mysql_select_db(db_database, $connection); # Połączenie z bazą o nazwie zadeklarowanej w "db_database".
-echo "test w dbconnect";
+echo "test w dbconnect3";
 ?>
