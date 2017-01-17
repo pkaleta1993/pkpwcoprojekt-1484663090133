7b240c2a89da8106f330a1640b79de1b326059b1
Paweł Kaleta <pkaleta1993@gmail.com>
2017-01-17 19:13:07 +0000

    h

--- install/test.php
+++ install/test.php
@@ -7,12 +7,12 @@ $password = "Jg*4KM81=<5pM";
 $conn = mysqli_connect($servername, $username, $password);
 
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-<<<<<<< Upstream, based on f4ab9de958dc08b49ab2d9d2cbdab554f2ff70a7
-=======
-
->>>>>>> a29200b c2
+//if (!$conn) {
+  //  die("Connection failed: " . mysqli_connect_error());
+//}
+//<<<<<<< Upstream, based on f4ab9de958dc08b49ab2d9d2cbdab554f2ff70a7
+//=======
+//
+//>>>>>>> a29200b c2
 echo "Connected successfully";
 ?>
