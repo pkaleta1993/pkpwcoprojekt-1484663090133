5df38a9eb1ed984252c22d67f71e89af952eb747
Paweł Kaleta <pkaleta1993@gmail.com>
2017-01-17 19:04:17 +0000

    c2

--- install/test.php
+++ install/test.php
@@ -10,5 +10,9 @@ $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+<<<<<<< Upstream, based on f4ab9de958dc08b49ab2d9d2cbdab554f2ff70a7
+=======
+
+>>>>>>> a29200b c2
 echo "Connected successfully";
 ?>
