f4ab9de958dc08b49ab2d9d2cbdab554f2ff70a7
Paweł Kaleta <pkaleta1993@gmail.com>
2017-01-17 18:59:47 +0000

    c

--- install/test.php
+++ install/test.php
@@ -1,4 +1,14 @@
 <?php
-echo "test";
+$servername = "lamp.ii.us.edu.pl";
+$username = "ii292684";
+$password = "Jg*4KM81=<5pM";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
-testowanie
