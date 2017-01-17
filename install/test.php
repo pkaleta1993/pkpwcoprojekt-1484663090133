bc0640fd6715aef5706089815f147bdb3a82eb1b
Paweł Kaleta <pkaleta1993@gmail.com>
2017-01-17 19:18:56 +0000

    ctest

--- install/test.php
+++ install/test.php
@@ -4,7 +4,7 @@ $username = "ii292684";
 $password = "Jg*4KM81=<5pM";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysql_connect($servername, $username, $password);
 
 // Check connection
 //if (!$conn) {
